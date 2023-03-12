6136d31c110f4ee32cbe6970b870118ef3f59f84
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-12 17:32:52 +0800

    - update src/Main.swift

--- src/Main.swift
+++ src/Main.swift
@@ -1,30 +1,10 @@
 import SwiftUI
 
 @main
-class Main: App
-{
-/*    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    var the_view_: ContentView!
-    
-    */
-   /*
-     @Environment(\.scenePhase) var scenePhase
-    */
-    var body: some Scene
-    {
-        WindowGroup
-        {
+struct Main: App {
+    var body: some Scene {
+        WindowGroup {
             ContentView()
-        }
-    }
-}
-
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Hello, World!")
-            Text("I'm great!")
         }
     }
 }
